e4bde62e5583ce59ae1d4639f1e0184855e0142f
Anna Smyrnova <anuto4ka777s@gmail.com>
2025-07-14 21:08:38 +0300

    done task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,11 @@
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+button.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+});
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
